2c4bdd298caf941a7eb985bba2b1f7ee59644b78
Nick Krichevsky <njk828@gmail.com>
2016-07-05 15:59:34 -0400

    Fix typo in cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -5,7 +5,7 @@ if [ -z $POSTGRES_USERNAME ]; then
 fi
 
 if [ -z $POSTGRES_LISTEN_ADDRESS ]; then
-	POSTGRES_LISTEN_ADDRESS="0.0.0.0/0"	
+	POSTGRES_LISTEN_ADDRESS="0.0.0.0/0"
 elif [[ ! $POSTGRES_LISTEN_ADDRESS =~ .*\/[0-9]{1,2}$ ]]; then
 	POSTGRES_LISTEN_ADDRESS="$POSTGRES_LISTEN_ADDRESS/0"
 fi
@@ -25,20 +25,20 @@ echo "Initializing role $POSTGRES_USERNAME..."
 service postgresql start
 if [ -z $POSTGRES_PASSWORD ]; then
 	sudo -u postgres psql -c "DO \$\$ BEGIN IF NOT EXISTS(SELECT * FROM pg_catalog.pg_user WHERE usename='$POSTGRES_USERNAME') THEN CREATE ROLE root SUPERUSER LOGIN; END IF; END \$\$;"
-	echo "Initialisd role $POSTGRES_USERNAME without passoword. Be warned: Anyone will be able to connect to this."
+	echo "Initialisd role $POSTGRES_USERNAME without password. Be warned: Anyone will be able to connect to this."
 	#Allow connections with no auth
-	connString="host all all $POSTGRES_LISTEN_ADDRESS trust" 
+	connString="host all all $POSTGRES_LISTEN_ADDRESS trust"
 
 else
 	sudo -u postgres psql -c "DO \$\$ BEGIN IF NOT EXISTS(SELECT * FROM pg_catalog.pg_user WHERE usename='$POSTGRES_USERNAME') THEN CREATE ROLE root SUPERUSER LOGIN PASSWORD '$POSTGRES_PASSWORD'; END IF; END \$\$;"
 	echo "Initialized role $POSTGRES_USERNAME with password."
 	#Allow connections with auth
-	connString="host all all $POSTGRES_LISTEN_ADDRESS md5" 
+	connString="host all all $POSTGRES_LISTEN_ADDRESS md5"
 fi
 service postgresql stop;
 
 echo "Setting up network addresses..."
-#If our connString isn't already in there, add it in. 
+#If our connString isn't already in there, add it in.
 if !(grep "$connString" /etc/postgresql/9.4/main/pg_hba.conf) then
  echo $connString >> /etc/postgresql/9.4/main/pg_hba.conf;
 fi
