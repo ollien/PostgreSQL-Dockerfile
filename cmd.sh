467ee8bff8ea5ac0c6a62e3465329e40cb3422f1
Nick Krichevsky <nick@ollien.com>
2017-09-25 12:06:11 -0400

    Fix bug where setting a different usenrame wouldn't work

--- cmd.sh
+++ cmd.sh
@@ -24,13 +24,13 @@ echo "Initializing role $POSTGRES_USERNAME..."
 
 service postgresql start
 if [ -z $POSTGRES_PASSWORD ]; then
-	sudo -u postgres psql -c "DO \$\$ BEGIN IF NOT EXISTS(SELECT * FROM pg_catalog.pg_user WHERE usename='$POSTGRES_USERNAME') THEN CREATE ROLE root SUPERUSER LOGIN; END IF; END \$\$;"
+	sudo -u postgres psql -c "DO \$\$ BEGIN IF NOT EXISTS(SELECT * FROM pg_catalog.pg_user WHERE usename='$POSTGRES_USERNAME') THEN CREATE ROLE $POSTGRES_USERNAME SUPERUSER LOGIN; END IF; END \$\$;"
 	echo "Initialized role $POSTGRES_USERNAME without password. Be warned: Anyone will be able to connect to this."
 	#Allow connections with no auth
 	connString="host all all $POSTGRES_LISTEN_ADDRESS trust"
 
 else
-	sudo -u postgres psql -c "DO \$\$ BEGIN IF NOT EXISTS(SELECT * FROM pg_catalog.pg_user WHERE usename='$POSTGRES_USERNAME') THEN CREATE ROLE root SUPERUSER LOGIN PASSWORD '$POSTGRES_PASSWORD'; END IF; END \$\$;"
+	sudo -u postgres psql -c "DO \$\$ BEGIN IF NOT EXISTS(SELECT * FROM pg_catalog.pg_user WHERE usename='$POSTGRES_USERNAME') THEN CREATE ROLE $POSTGRES_USERNAME SUPERUSER LOGIN PASSWORD '$POSTGRES_PASSWORD'; END IF; END \$\$;"
 	echo "Initialized role $POSTGRES_USERNAME with password."
 	#Allow connections with auth
 	connString="host all all $POSTGRES_LISTEN_ADDRESS md5"
